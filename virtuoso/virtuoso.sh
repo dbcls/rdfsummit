4cbbb91d5a9cc203db3e17640e25b07c534afae5
Toshiaki Katayama <k@bioruby.org>
2019-10-30 10:02:31 +0900

    Supported paths for Mac and Windows binary packages. Greatly improved time for listing graph names via SQL

--- virtuoso/virtuoso.sh
+++ virtuoso/virtuoso.sh
@@ -1,58 +1,92 @@
 #!/bin/sh
 
-prefix=/opt/virtuoso
+### Parameters to be redefined
+
 port=1111
 user=dba
 pass=dba
 
-isql="${prefix}/bin/isql ${port} ${user} ${pass}"
+## Default for the source code distribution
 
-## To use the Virtuoso OS X binary package, link the application directory to ${prefix} and change ${dbdir} and ${dbfile}.
-## % ln -s /Applications/Virtuoso\ Open\ Source\ Edition\ v7.2.app/Contents/virtuoso-opensource /opt/virtuoso
-
-#dbdir="${prefix}/database"
-#dbfile="database"
-
+prefix=/opt/virtuoso
 dbdir="${prefix}/var/lib/virtuoso/db"
 dbfile="virtuoso"
 
+## Parameters for binary packages
+
+# Download from (as of release v7.2.5.1)
+#   * https://github.com/openlink/virtuoso-opensource/releases
+# Linux
+#   * virtuoso-opensource.x86_64-generic_glibc25-linux-gnu.tar.gz
+# Mac OS X (macOS)
+#   * virtuoso-opensource-7.2.5-macosx-app.dmg
+# Windows
+#   * Virtuoso_OpenSource_Server_7.20.x64.exe
+
+# To use the Virtuoso Linux binary package, set the installed application directory to ${prefix} then change ${dbdir} and ${dbfile}.
+#   % sudo mv virtuoso-opensource /opt/virtuoso
+
+#prefix=/opt/virtuoso-opensource
+#dbdir="${prefix}/database"
+#dbfile="virtuoso"
+
+# To use the Virtuoso OS X binary package, set the installed application directory to ${prefix} then change ${dbdir} and ${dbfile}.
+#   % sudo ln -s /Applications/Virtuoso\ Open\ Source\ Edition\ v7.2.app/Contents/virtuoso-opensource /opt/virtuoso
+
+#prefix="/Applications/Virtuoso Open Source Edition v7.2.app/Contents/virtuoso-opensource"
+#dbdir="${prefix}/database"
+#dbfile="database"
+
+# To use the Virtuoso Windows binary package, set the installed application directory to ${prefix} then change ${dbdir} and ${dbfile}.
+#   % sudo ln -s '/mnt/c/Program Files/OpenLink Software/Virtuoso OpenSource 7.20/' /opt/virtuoso
+# Unfortunately, it seems that linking from /mnt/c (Windows volume) to /opt (Ubuntsu volume) makes exe files unexecutable, thus it would be better to point the installed directory.
+
+#prefix="/mnt/c/Program Files/OpenLink Software/Virtuoso OpenSource 7.20/"
+#dbdir="${prefix}/database"
+#dbfile="virtuoso"
+
+### End of parameters
+
+
+isql="${prefix}/bin/isql${ext}"
+opts="${port} ${user} ${pass}"
 
 case $1 in
     start)
-        (cd ${dbdir}; ${prefix}/bin/virtuoso-t)
+        (cd "${dbdir}"; "${prefix}/bin/virtuoso-t${ext}")
         ;;
     stop)
-        echo "shutdown;" | ${isql}
+        echo "shutdown;" | "${isql}" ${opts}
         ;;
     status)
         echo "isql ${port}"
-        echo "status();" | ${isql}
+        echo "status();" | "${isql}" ${opts}
         echo
         ;;
     isql)
-        ${isql}
+        "${isql}" ${opts}
         ;;
     port)
-        echo ${port}
+        echo "${port}"
         ;;
     path)
-        echo ${dbdir}
+        echo "${dbdir}"
         ;;
     dir)
-        ls -l ${dbdir}
+        ls -l "${dbdir}"
         ;;
     log)
-        tail -f ${dbdir}/${dbfile}.log
+        tail -f "${dbdir}/${dbfile}.log"
         ;;
     edit)
-        ${EDITOR:-vi} ${dbdir}/virtuoso.ini
+        ${EDITOR:-vi} "${dbdir}/virtuoso.ini"
         ;;
     clear)
         read -p "Deleate all data. Continue? (Yes/No): " answer
         if [ "${answer:-No}" = "Yes" ]; then
-          mv ${dbdir}/virtuoso.ini ${prefix}/virtuoso.ini
-          rm -f ${dbdir}/*
-          mv ${prefix}/virtuoso.ini ${dbdir}/virtuoso.ini
+          mv "${dbdir}/virtuoso.ini" "${prefix}/virtuoso.ini"
+          rm -f "${dbdir}"/*
+          mv "${prefix}/virtuoso.ini" "${dbdir}/virtuoso.ini"
         else
           echo "Aborted."
         fi
@@ -62,14 +96,14 @@ case $1 in
           log_enable(2,1);
           DB.DBA.RDF_LOAD_RDFXML_MT(file_to_string_output('$3'), '', '$2');
           checkpoint;
-        " | ${isql}
+        " | "${isql}" ${opts}
         ;;
     loadttl)
         echo "
           log_enable(2,1);
           DB.DBA.TTLP_MT(file_to_string_output('$3'), '', '$2', 337);
           checkpoint;
-        " | ${isql}
+        " | "${isql}" ${opts}
         ;;
     loaddir)
         echo "
@@ -77,48 +111,58 @@ case $1 in
           ld_dir_all('$3', '$4', '$2');
           rdf_loader_run();
           checkpoint;
-        " | ${isql}
+        " | "${isql}" ${opts}
         ;;
     addloader)
-        echo "rdf_loader_run();" | ${isql} &
+        echo "rdf_loader_run();" | "${isql}" ${opts} &
         ;;
     watch)
-        echo "SELECT \
-                CASE ll_state \
-                  WHEN 0 THEN 'Waiting' \
-                  WHEN 1 THEN 'Loading' \
-                  WHEN 2 THEN 'Done' \
-                  ELSE 'Unknown' \
-                END AS status, \
-                COUNT(*) \
-                FROM DB.DBA.LOAD_LIST \
-                GROUP BY ll_state;" | ${isql}
+        echo "
+          SELECT \
+            CASE ll_state \
+              WHEN 0 THEN 'Waiting' \
+              WHEN 1 THEN 'Loading' \
+              WHEN 2 THEN 'Done' \
+              ELSE 'Unknown' \
+            END AS status, \
+            COUNT(*) \
+          FROM DB.DBA.LOAD_LIST \
+          GROUP BY ll_state;
+        " | "${isql}" ${opts}
         ;;
     watch_wait)
-        echo "SELECT ll_graph, ll_file \
-          FROM DB.DBA.LOAD_LIST WHERE ll_state = 0;" \
-            | ${isql} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
+        echo "
+          SELECT ll_graph, ll_file \
+          FROM DB.DBA.LOAD_LIST \
+          WHERE ll_state = 0;
+        " | "${isql}" ${opts} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
         ;;
     watch_load)
-        echo "SELECT ll_graph, ll_file, ll_started \
-          FROM DB.DBA.LOAD_LIST WHERE ll_state = 1;" \
-            | ${isql} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
+        echo "
+          SELECT ll_graph, ll_file, ll_started \
+          FROM DB.DBA.LOAD_LIST \
+          WHERE ll_state = 1;
+        " | "${isql}" ${opts} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
         ;;
     watch_done)
-        echo "SELECT ll_graph, ll_file, ll_started, (ll_done - ll_started) AS duration \
-          FROM DB.DBA.LOAD_LIST WHERE ll_state = 2;" \
-            | ${isql} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
+        echo "
+          SELECT ll_graph, ll_file, ll_started, (ll_done - ll_started) AS duration \
+          FROM DB.DBA.LOAD_LIST \
+          WHERE ll_state = 2;
+        " | "${isql}" ${opts} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
         ;;
     watch_error)
-        echo "SELECT ll_graph, ll_file, ll_started, (ll_done - ll_started) AS duration, ll_error \
-          FROM DB.DBA.LOAD_LIST WHERE ll_error IS NOT NULL;" \
-            | ${isql} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
+        echo "
+          SELECT ll_graph, ll_file, ll_started, (ll_done - ll_started) AS duration, ll_error \
+          FROM DB.DBA.LOAD_LIST \
+          WHERE ll_error IS NOT NULL;
+        " | "${isql}" ${opts} | perl -ne 's/  +/\t/g; print if /^(SQL> ll|http)/#'
         ;;
     list)
-        echo "SPARQL SELECT ?g WHERE { GRAPH ?g {[] [] []} } GROUP BY ?g ORDER BY ?g;" | ${isql}
+        echo "SELECT * FROM SPARQL_SELECT_KNOWN_GRAPHS_T ORDER BY GRAPH_IRI;" | "${isql}" ${opts}
         ;;
     head)
-        echo "SPARQL SELECT DISTINCT * WHERE { GRAPH <$2> {?s ?p ?o} } LIMIT 10;" | ${isql}
+        echo "SPARQL SELECT DISTINCT * WHERE { GRAPH <$2> {?s ?p ?o} } LIMIT 10;" | "${isql}" ${opts}
         ;;
     drop)
         read -p "Deleate all data in the graph '$2'. Continue? (Yes/No): " answer
@@ -127,15 +171,15 @@ case $1 in
             log_enable(2,1);
             SPARQL CLEAR GRAPH <$2>;
             checkpoint;
-          " | ${isql}
-          echo "SPARQL SELECT COUNT(*) FROM <$2> WHERE {?s ?p ?o};" | ${isql}
-          echo "DELETE FROM DB.DBA.LOAD_LIST WHERE ll_graph = '$2';" | ${isql}
+          " | "${isql}" ${opts}
+          echo "SPARQL SELECT COUNT(*) FROM <$2> WHERE {?s ?p ?o};" | "${isql}" ${opts}
+          echo "DELETE FROM DB.DBA.LOAD_LIST WHERE ll_graph = '$2';" | "${isql}" ${opts}
         else
           echo "Aborted."
         fi
         ;;
     query)
-        echo "SPARQL $2 ;" | ${isql}
+        echo "SPARQL $2 ;" | "${isql}" ${opts}
         ;;
     help)
         echo "Usage:"
